4229d065c6f25478aa30dbf87dbe18eaddfd0d59
nikzzxiter <nikzzxit@gmail.com>
2025-09-06 07:47:26 +0700

    Update fishithub.lua

--- fishithub.lua
+++ fishithub.lua
@@ -1,14 +1,5 @@
 local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
-
-local Window = OrionLib:MakeWindow({Name = "Test UI | Fish It Hub", IntroText = "Test Loaded"})
-
-local Tab = Window:MakeTab({Name = "Main", Icon = "rbxassetid://4483345998"})
-
-Tab:AddButton({
-    Name = "Test Button",
-    Callback = function()
-        OrionLib:MakeNotification({Name="FishItHub", Content="UI Works!", Time=5})
-    end
-})
-
+local Window = OrionLib:MakeWindow({Name="Test UI", IntroText="Orion Works!"})
+local Tab = Window:MakeTab({Name="Main"})
+Tab:AddButton({Name="Click Me", Callback=function() OrionLib:MakeNotification({Name="Test", Content="UI Muncul!", Time=3}) end})
 OrionLib:Init()
